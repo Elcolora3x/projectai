d6c47d3f9b3e00b97b8229e00ad097c329a34cf3
Elcolora3x <elcolora2x@gmail.com>
2026-03-22 17:45:53 -0300

    initial commit

---
+++ src/main/java/com/elcolora2x/dev/projectai/ProjectaiApplication.java
@@ -0,0 +1,13 @@
+package com.elcolora2x.dev.projectai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elcolora2x/dev/projectai/ProjectaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcolora2x.dev.projectai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
