c42911866fd52473d7984afee74535975d75078e
warps0 <matias.trossero.1@gmail.com>
2026-03-27 20:02:29 -0300

    feature: JPA Implementations

---
+++ src/main/java/com/elcolora2x/dev/projectai/repository/AssistanceRepository.java
@@ -0,0 +1,13 @@
+package com.elcolora2x.dev.projectai.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.elcolora2x.dev.projectai.entity.model.Assistance;
+
+@Repository
+public interface AssistanceRepository extends JpaRepository<Assistance, UUID> {
+
+}
